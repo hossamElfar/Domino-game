81087c95b49244f20ce6a6a55689fcff712a4a1e
Hossam Ahmed <hossam.elfar95@gmail.com>
2017-04-27 16:15:00 +0300

    initial commit

---
+++ controllers/board.js
@@ -0,0 +1,199 @@
+var Deque = require("double-ended-queue");
+const uuidV4 = require('uuid/v4');
+module.exports = {
+  
+   /* Create the dominos cards */
+    createDominos: function(cb) {
+        var returned = [];
+        for (var i = 0; i < 7; i++) {
+            for (var j = 0; j < 7; j++) {
+                var tile = {
+                    "front": i,
+                    "back": j,
+                    "uuid": uuidV4()
+                }
+                returned.push(tile)
+            }
+        }
+        cb(returned);
+    },
+
+    /* Initialize a new game instance  */
+    initGame: function(cb) {
+        var players = [];
+        this.createDominos((dominos) => {
+            for (var i = 0; i < 4; i++) {
+                dominos = dominos.sort(function() {
+                    return 0.5 - Math.random()
+                });
+                if (i != 3) {
+                    var playerTiles = [];
+                    for (var j = 0; j < 7; j++) {
+                        playerTiles.push(dominos[j]);
+                        dominos.splice(j, 1);
+                    }
+                    var player = {
+                        "name": "computer " + (i + 1),
+                        "tiles": playerTiles
+                    }
+                    players.push(player);
+                } else {
+                    var playerTiles = [];
+                    for (var j = 0; j < 7; j++) {
+                        playerTiles.push(dominos[j]);
+                        dominos.splice(j, 1);
+                    }
+                    var player = {
+                        "name": "you",
+                        "tiles": playerTiles
+                    }
+                    players.push(player);
+                }
+
+            }
+            var game = {
+                "players": players,
+                "remaining_tile": dominos,
+                "board": [],
+                "turn": players[0]
+            }
+            cb(game);
+        });
+
+    },
+
+    /* Play a tile  */
+    play: function(tile, game, cb) {
+        var board = game.game.board;
+        var player = game.game.turn;
+        var deque = new Deque();
+        deque.push(board);
+        if (board.length == 0) {
+            deque.push(tile);
+            var updated_board = deque.toArray()[1];
+            game.game.board.push(updated_board);
+            var player_tiles = player.tiles;
+            player_tiles.forEach((tile_p) => {
+                if (tile_p.uuid == tile.uuid) {
+                    var index = player_tiles.indexOf(tile_p.uuid);
+                    player_tiles.splice(index, 1);
+                }
+            })
+            var next_player = this.next(player, game);
+            game.game.turn = next_player;
+            cb(game, false, true)
+        } else {
+            var front = deque.peekFront();
+            var back = deque.peekBack();
+            if (front[0].front == tile.front || front[0].front == tile.back) {
+              var player_tiles = player.tiles;
+              player_tiles.forEach((tile_p) => {
+                  if (tile_p.uuid == tile.uuid) {
+                      var index = player_tiles.indexOf(tile_p.uuid);
+                      player_tiles.splice(index, 1);
+                  }
+              })
+                deque.unshift(tile);
+                console.log(deque.toArray());
+                var updated_board = deque.toArray()[0];
+
+                game.game.board.push(updated_board);
+                var winner = this.isWinner(player);
+                var next_player = this.next(player, game);
+                game.game.turn = next_player;
+                cb(game, winner, true)
+            } else {
+                if (back[0].back == tile.front || back[0].back == tile.back) {
+                    var updated_board = deque.toArray()[0][0];
+                    console.log("heree2 " + deque.toArray()[0][0]);
+                    var player_tiles = player.tiles;
+                    player_tiles.forEach((tile_p) => {
+                        if (tile_p.uuid == tile.uuid) {
+                            var index = player_tiles.indexOf(tile_p.uuid);
+                            player_tiles.splice(index, 1);
+                        }
+                    })
+                    game.game.board.push(updated_board);
+                    var winner = this.isWinner(player);
+                    var next_player = this.next(player, game);
+                    game.game.turn = next_player;
+                    cb(game, winner, true)
+                } else {
+                    var next_player = this.next(player, game);
+                    game.game.turn = next_player;
+                    cb(game, false, false);
+                }
+            }
+        }
+    },
+
+    /* Check if a player can play or not */
+    canPlay: function(tiles, game, cb) {
+        var board = game.game.board;
+        var deque = new Deque();
+        deque.push(board);
+        var flag = false;
+        tiles.forEach((tile) => {
+            if (board.length == 0) {
+                deque.push(tile);
+                var updated_board = deque.toArray()[1];
+                game.game.board.push(updated_board);
+                flag = true;
+                cb(true);
+
+
+            } else {
+                var front = deque.peekFront();
+                var back = deque.peekBack();
+                if (front[0].front == tile.front || front[0].front == tile.back) {
+                    deque.shift(tile);
+                    var updated_board = deque.toArray()[0][0];
+                    console.log("heree1 " + deque.toArray()[0]);
+                    game.game.board.push(updated_board);
+                    flag = true;
+                    cb(true);
+
+                } else {
+                    if (back[0].back == tile.front || back[0].back == tile.back) {
+                        var updated_board = deque.toArray()[0][0];
+                        console.log("heree2 " + deque.toArray()[0][0]);
+                        game.game.board.push(updated_board);
+                        flag = true;
+                        cb(true);
+
+                    }
+                }
+            }
+        })
+        cb(flag);
+    },
+
+    /* Check if the player is winner or not */
+    isWinner: function(player) {
+        if (player.tiles.length == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+
+    /* Chaniging the turns after a player played */
+    next: function(player, game) {
+        var players = game.game.players;
+        var index = -1
+        if (player.name.includes("1")) {
+            index = 1;
+        }
+        if (player.name.includes("2")) {
+            index = 2;
+        }
+        if (player.name.includes("3")) {
+            index = 3;
+        }
+        if (player.name.includes("you")) {
+            index = 0;
+        }
+        return players[index];
+    }
+
+}
---
+++ routes/index.js
@@ -0,0 +1,89 @@
+var express = require('express');
+var router = express.Router();
+var Board = require('../controllers/board.js');
+var Deque = require("double-ended-queue");
+var db = require('../db,js');
+var ObjectId = require('mongodb').ObjectID;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index');
+});
+/* Craete a new game instance  */
+router.get('/create-board', (req, res) => {
+    Board.initGame((data) => {
+        db.connect((err, db) => {
+            if (!err) {
+                var collection = db.collection("games");
+                // Insert a single document
+                collection.insertOne({
+                    "game": data
+                }, (err, result) => {
+                    collection.findOne({
+                        "game": data
+                    }, function(err, item) {
+                      res.json(item);
+                    })
+                });
+            }
+        });
+
+    });
+});
+
+/* Play a tile  */
+router.post('/play/:id', (req, res) => {
+    var id = req.params.id;
+    var tile = req.body.tile;
+
+    db.connect((err, db) => {
+        if (!err) {
+            var collection = db.collection("games");
+            collection.findOne({
+                "_id": new ObjectId(id)
+            }, function(err, game) {
+                Board.play(tile, game, (game_updated,winner,state) => {
+                    if (state == true) {
+                      console.log(game_updated);
+                      collection.updateOne({"_id": new ObjectId(id)},game_updated);
+                      //console.log(game.game.board[4]);
+                      if(winner){
+                        res.json({'state':"winner !!!"});
+                      }else{
+                          res.json(game);
+                      }
+                    } else {
+                        res.json({"state": "you cannot play"});
+                    }
+                })
+            })
+
+        }
+    });
+});
+
+/* Check whether the player can play or not */
+router.post('/canPlay/:id', (req, res) => {
+    var id = req.params.id;
+    var tiles = req.body.tiles;
+
+    db.connect((err, db) => {
+        if (!err) {
+            var collection = db.collection("games");
+            collection.findOne({
+                "_id": new ObjectId(id)
+            }, function(err, game) {
+                Board.canPlay(tiles, game, (state) => {
+                    if (state == true) {
+                        res.json({"state": "you can play"});
+                    } else {
+                      res.json({"state": "you cannot play"});
+                    }
+                })
+            })
+
+        }
+    });
+});
+
+module.exports = router;
